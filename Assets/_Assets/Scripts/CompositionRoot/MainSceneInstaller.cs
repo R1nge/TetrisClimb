2f65302f4d31c553959f9d23cd545e4335f09cc4
R1nge <r1ngegamedev@gmail.com>
2024-10-06 07:45:10 +0300

    removed states

--- Assets/_Assets/Scripts/CompositionRoot/MainSceneInstaller.cs
+++ Assets/_Assets/Scripts/CompositionRoot/MainSceneInstaller.cs
@@ -1,11 +1,8 @@
-using _Assets.Scripts.Gameplay.Inputs;
 using _Assets.Scripts.Services.Factories;
 using _Assets.Scripts.Services.StateMachine;
 using _Assets.Scripts.Services.StateMachine.StatesCreators;
 using _Assets.Scripts.Services.UIs;
 using _Assets.Scripts.Services.UIs.StateMachine;
-using _Assets.Scripts.Services.UIs.StatesCreators;
-using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
@@ -19,12 +16,11 @@ namespace _Assets.Scripts.CompositionRoot
             builder.Register<PlayerFactory>(Lifetime.Singleton);
             builder.Register<TetrisFactory>(Lifetime.Singleton);
             //
-            
+
             builder.Register<MainMenuUIStatesFactory>(Lifetime.Singleton);
             builder.Register<MainMenuUIFactory>(Lifetime.Singleton);
             builder.Register<MainMenuStatesFactory>(Lifetime.Singleton);
 
-            builder.Register<UIMainSceneStateCreator>(Lifetime.Singleton);
             builder.Register<MainSceneStateCreator>(Lifetime.Singleton);
         }
     }
--- Assets/_Assets/Scripts/Services/StateMachine/StatesCreators/MainSceneStateCreator.cs
+++ Assets/_Assets/Scripts/Services/StateMachine/StatesCreators/MainSceneStateCreator.cs
@@ -1,5 +1,4 @@
 ﻿using _Assets.Scripts.Services.StatesCreator;
-using _Assets.Scripts.Services.UIs;
 using _Assets.Scripts.Services.UIs.StateMachine;
 
 namespace _Assets.Scripts.Services.StateMachine.StatesCreators
@@ -11,27 +10,35 @@ namespace _Assets.Scripts.Services.StateMachine.StatesCreators
         private readonly MainMenuUIStatesFactory _mainMenuUIStatesFactory;
         private readonly UIStateMachine _uiStateMachine;
 
-        private MainSceneStateCreator(GameStateMachine gameStateMachine, UIStateMachine uiStateMachine, MainMenuStatesFactory mainMenuStatesFactory, MainMenuUIStatesFactory mainMenuUIStatesFactory )
+        private MainSceneStateCreator(GameStateMachine gameStateMachine, UIStateMachine uiStateMachine,
+            MainMenuStatesFactory mainMenuStatesFactory, MainMenuUIStatesFactory mainMenuUIStatesFactory)
         {
             _gameStateMachine = gameStateMachine;
             _uiStateMachine = uiStateMachine;
             _mainMenuStatesFactory = mainMenuStatesFactory;
             _mainMenuUIStatesFactory = mainMenuUIStatesFactory;
         }
-        
+
         public void Init()
         {
-            _gameStateMachine.AddState(GameStateType.Init, _mainMenuStatesFactory.CreateAsyncState(GameStateType.Init, _gameStateMachine));
-            _gameStateMachine.AddState(GameStateType.Game, _mainMenuStatesFactory.CreateAsyncState(GameStateType.Game, _gameStateMachine));
-            
-            _uiStateMachine.AddState(UIStateType.Loading, _mainMenuUIStatesFactory.CreateState(UIStateType.Loading, _uiStateMachine));
-            _uiStateMachine.AddState(UIStateType.Game, _mainMenuUIStatesFactory.CreateState(UIStateType.Game, _uiStateMachine));
+            _gameStateMachine.AddState(GameStateType.Init,
+                _mainMenuStatesFactory.CreateAsyncState(GameStateType.Init, _gameStateMachine));
+            _gameStateMachine.AddState(GameStateType.Game,
+                _mainMenuStatesFactory.CreateAsyncState(GameStateType.Game, _gameStateMachine));
+
+            _uiStateMachine.AddState(UIStateType.Loading,
+                _mainMenuUIStatesFactory.CreateState(UIStateType.Loading, _uiStateMachine));
+            _uiStateMachine.AddState(UIStateType.Game,
+                _mainMenuUIStatesFactory.CreateState(UIStateType.Game, _uiStateMachine));
         }
 
         public void Dispose()
         {
-           _gameStateMachine.RemoveState(GameStateType.Init);
-           _gameStateMachine.RemoveState(GameStateType.Game);
+            _gameStateMachine.RemoveState(GameStateType.Init);
+            _gameStateMachine.RemoveState(GameStateType.Game);
+
+            _uiStateMachine.RemoveState(UIStateType.Loading);
+            _uiStateMachine.RemoveState(UIStateType.Game);
         }
     }
 }
